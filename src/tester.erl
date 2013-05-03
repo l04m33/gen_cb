9680b3688af048965c1067b300e0fe5fbaad00e7
Kay Zheng <l04m33@gmail.com>
2013-05-03 08:12:55 +0000

    Update the sample code

--- src/tester.erl
+++ src/tester.erl
@@ -1,12 +1,44 @@
 -module(tester).
 -behaviour(gen_cb).
 
+-include("gen_cb.hrl").
+
+-export([
+    start/0,
+    start_link/0,
+    do_sync_call/1,
+    send_async_msg/1,
+    get_fixed_reply/1]).
+
 -export([
     init/1,
     handle_call/3,
     handle_info/2,
     terminate/2]).
 
+%%% -----------------------------------------------------------------
+%%% APIs
+%%% -----------------------------------------------------------------
+start() ->
+    gen_cb:start({local, ?MODULE}, ?MODULE, [dummy_arg], []).
+
+start_link() ->
+    gen_cb:start_link({local, ?MODULE}, ?MODULE, [dummy_arg], []).
+
+
+do_sync_call(Msg) ->
+    gen_cb:call(?MODULE, Msg, fun gen_cb:receive_cb/1, fun gen_cb:reply_cb/1).
+
+send_async_msg(Msg) ->
+    gen_cb:call(?MODULE, Msg, none, none).
+
+get_fixed_reply(Msg) ->
+    gen_cb:call(?MODULE, Msg, fun gen_cb:receive_cb/1, fun fix_reply/1).
+
+
+%%% -----------------------------------------------------------------
+%%% Callbacks
+%%% -----------------------------------------------------------------
 init(Args) ->
     erlang:process_flag(trap_exit, true),
     io:format("init, Args = ~w~n", [Args]),
@@ -35,4 +67,13 @@ terminate(Reason, State) ->
     io:format("terminate, Reason = ~w, State = ~w~n",
               [Reason, State]),
     ok.
+
+
+%%% -----------------------------------------------------------------
+%%% Privates
+%%% -----------------------------------------------------------------
+fix_reply(CBEvent) ->
+    Reply = CBEvent#cb_event.cb_arg,
+    NCBEvent = CBEvent#cb_event{cb_arg = lists:duplicate(2, Reply)},
+    gen_cb:reply_cb(NCBEvent).
 
